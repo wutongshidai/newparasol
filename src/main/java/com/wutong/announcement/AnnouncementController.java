5066a5b9d44f8e9eb6295c557c475f0ec74f3628
aimeryaya <117468082@qq.com>
2018-01-12 10:13:12 +0800

    新增公告管理

---
+++ src/main/java/com/wutong/announcement/AnnouncementController.java
@@ -0,0 +1,40 @@
+package com.wutong.announcement;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.github.pagehelper.PageInfo;
+import com.parasol.core.announcement.Announcement;
+import com.parasol.core.service.AnnouncementService;
+import com.wutong.framework.core.web.common.http.ResponseResult;
+
+@RestController
+@RequestMapping("/announcement")
+public class AnnouncementController {
+
+	private static final int DEFAULT_PAGE_SIZE = 10;
+	
+	@Reference
+	private AnnouncementService announcementService;
+	
+	/**
+	 * 查询公告列表
+	 * @param columnId
+	 * @param status
+	 * @param pageNo
+	 * @param pageSize
+	 * @return
+	 */
+	@RequestMapping(path="")
+	public ResponseResult<PageInfo<Announcement>> index(Integer columnId, Integer status, Integer pageNo, Integer pageSize) {
+		columnId = (columnId == null ? -1 : columnId);
+		pageSize = (pageSize == null || pageSize == 0? DEFAULT_PAGE_SIZE : pageSize);
+		pageNo = (pageNo == null || pageNo == 0 ? 1 : pageNo);
+		status = (status == null ? -1 : status);
+		PageInfo<Announcement> announcements = announcementService.list(columnId, status, pageNo, pageSize);
+		ResponseResult<PageInfo<Announcement>> pageInfo = new ResponseResult<>();
+		pageInfo.addData(announcements);
+		return pageInfo;
+	}
+}
--- src/main/java/com/wutong/user/UserManageController.java
+++ src/main/java/com/wutong/user/UserManageController.java
@@ -1,23 +1,40 @@
 package com.wutong.user;
 
+import java.lang.reflect.InvocationTargetException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.beanutils.BeanUtils;
+import org.apache.commons.codec.binary.Hex;
+import org.apache.log4j.Logger;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.parasol.common.converter.Validator;
+import com.parasol.core.Enum.TenderStatusEnum;
+import com.parasol.core.bid.Bid_info;
 import com.parasol.core.bid.Bid_order;
+import com.parasol.core.bid.OrderInfo;
 import com.parasol.core.bid.TenderBid;
 import com.parasol.core.service.BidService;
 import com.parasol.core.service.TenderService;
 import com.parasol.core.service.UserService;
 import com.parasol.core.tender.Tender;
 import com.parasol.core.user.User;
+import com.wutong.framework.core.web.auth.aop.annotation.AuthLogin;
 import com.wutong.framework.core.web.common.http.ResponseResult;
 
 import cn.itcast.common.page.Pagination;
@@ -25,6 +42,8 @@ import cn.itcast.common.page.Pagination;
 @RestController
 @RequestMapping("/userManage")
 public class UserManageController {
+	
+	private Logger logger = Logger.getLogger(this.getClass());
 
 	@Reference
 	private TenderService tenderService;
@@ -33,40 +52,65 @@ public class UserManageController {
 	@Reference
 	private BidService bidService;
 
-	/**
-	 * 我的发布(带分页)
-	 * @param request
-	 * @param page
-	 * @return
-	 * @throws Exception
-	 */
-	@RequestMapping("/alltenders")
-//  @AuthLogin
-	public ResponseResult alltenders(HttpServletRequest request , String page , String count) throws Exception{
-	 ResponseResult<Object> result = new ResponseResult<>();
-	 Map<String, Object> map = new HashMap<String, Object>();
-	 User user = (User) request.getSession().getAttribute("user");
-	  if(page == null){
-		 page= "1";
-	  }
-	 Pagination pagination = tenderService.tenderAll(user.getId() , Integer.parseInt(page) , Integer.parseInt(count));
-	 map.put("pagination", pagination);
-	 result.addData(map);
-	 return result;
-   }
+	 /**
+	  * 我的发布
+	  * @param classification 分类
+	  * @param userId  用户Id
+	  * @param count   每页条数
+	  * @param page    页码
+	  * @return
+	  */
+	 @RequestMapping("/selectListTender")
+	 @AuthLogin
+	 public ResponseResult selectListTender(String classification  , String count , String page , HttpServletRequest request){
+//		 User user = userService.selectByPrimaryKey(1);
+		 User user = (User) request.getSession().getAttribute("user");
+		 ResponseResult<Object> result = new ResponseResult<>();
+		 Map<?, ?> map = tenderService.selectListTender(classification , user.getId().toString() , count , page); 	
+		 result.addData(map);
+		 return result ;
+	 }
 	
+	/**
+	 * 删除发标信息
+	 * @param projectName
+	 * @return 0删除失败 ,1删除成功,2无删除权限
+	 */
+	 @RequestMapping("/deleteByneed")
+	 @AuthLogin
+	 public String deleteByneed(HttpServletRequest request , String tenderId){
+		 String flag = "0";
+//		 User user = (User) request.getSession().getAttribute("user");
+		 User user = userService.selectByPrimaryKey(1);
+		 Tender tender = tenderService.selectByPrimaryKey(Integer.parseInt(tenderId));
+//		 Tender tender = tenderService.selectByPrimaryName(urlDecode(projectName));
+		 if(tender.getUserid() == user.getId()){
+			 Boolean booleans = tenderService.deleteByPrinaryId(Integer.parseInt(tenderId));
+			 if(booleans == true){
+				 flag = "1";
+				 logger.info("标信息删除成功！");
+			 }
+		 }else{
+			 flag = "2";
+			 logger.info("无删除权限！");
+		 }
+		 return flag;
+	 }
+	 
 	/**
 	 * 我的投标查询(带条数)
 	 * @param com_userId
 	 * @return
 	 */
     @RequestMapping(value = "/getMyBid")
-//  @AuthLogin
-    public ResponseResult getMyBid (String id) {
-        int userId = Integer.parseInt(id);
+    @AuthLogin
+    public ResponseResult getMyBid (HttpServletRequest request ,String count , String page) {
+    	 User user = (User) request.getSession().getAttribute("user");
+//		 User user = userService.selectByPrimaryKey(1);
         Map map = new HashMap();
-        List<Bid_order> list = bidService.getMyBids(userId);
-        int count = bidService.countByUserId(userId);
+        List<Bid_order> list = bidService.getMyBids(user.getId().toString(),count,page);
+        System.out.println(list);
+        int count1 = bidService.countByUserId(user.getId());
         List  arr= new ArrayList();
         for (Bid_order bidOrder: list) {
             TenderBid tenderBid =new TenderBid();
@@ -74,51 +118,170 @@ public class UserManageController {
             Tender tender = tenderService.selectByPrimaryKey(tenderId);
             if (tender !=null){
                 String projectName = tender.getProjectName();
+                Date endDate = tender.getEndDate();
                 tenderBid.setBidOrder(bidOrder);
                 tenderBid.setProjectName(projectName);
+                tenderBid.setEndDate(endDate);
                 arr.add(tenderBid);
             }
         }
         map.put("Bid", arr);
-        map.put("count", count);
+        map.put("count", count1);
         ResponseResult responseResult = new ResponseResult();
         responseResult.addData(map);
         return responseResult;
     }
+    
+   /**
+    * 修改密码
+    * @param password
+    * @param newpassword
+    * @param request
+    * @return
+    */
+	@RequestMapping(value = "/updatePassword")
+    @AuthLogin
+	public ResponseResult updatePassword(String password, String newpassword ,
+			HttpServletRequest request) {
+		ResponseResult re = new ResponseResult();
+		User user = (User) request.getSession().getAttribute("user");
+		Map<String, Object> map = new HashMap<>();
+		String message="";
+			if (encodePassword(password).equals(user.getPassword())) {
+				user.setPassword(encodePassword(newpassword));
+				Boolean updateByNickNames = userService.updateByNickNames(user);
+				if (updateByNickNames){
+					message= "修改成功";
+					map.put("success", user.getUserName());
+					logger.info("密码修改成功！");
+				}else{
+					message = "未知错误,修改失败";
+					map.put("success", false);
+					logger.info("未知错误,修改失败！");
+				}
+				re.addData(map);
+			} else {
+				map.put("message", "初始密码错误");
+				map.put("success", false);
+				logger.info("初始密码错误！");
+				re.addData(map);
+			}
+			return re;	
+	}
 	
 	/**
 	 * 修改用户信息
 	 * @param companyName
-	 * @param id
 	 * @param request
 	 * @return
 	 */
 	@RequestMapping(value = "/updateMessage")
-//  @AuthLogin
-	public Map<String, Object> updateMessage(String companyName, String id , HttpServletRequest request) {
+    @AuthLogin
+	public ResponseResult<Object> updateMessage( HttpServletRequest request) {
 		User user = (User) request.getSession().getAttribute("user");
-		Map<String, Object> map = new HashMap<String, Object>();
-		map.put("success", false);
+//		User user = userService.selectByPrimaryKey(1);
+		ResponseResult<Object> result = new ResponseResult<>();
+//		User user = new User();
 		try {
-			if (companyName.trim() != null && companyName.length() != 0 && id != null && id != "") {
-				user.setCompanyName(companyName);
-				Integer parseInt = Integer.parseInt(id);
-				user.setId(parseInt);
-				Boolean updateByNickNames = userService.updateByNickNames(user);
-				map.put("success", updateByNickNames);
-				map.put("name", user.getCompanyName());
-				return map;
-			} else {
-				map.put("success", false);
-				map.put("message", "昵称或Id为空，修改失败！");
-				return map;
+			BeanUtils.populate(user, request.getParameterMap());
+			Boolean boolean1 = userService.updateByNickNames(user);
+			if(boolean1){
+				User key = userService.selectByPrimaryKey(user.getId());
+				result.addData(key);
+				logger.info("更新成功！");
+			}else{
+				result.addData("更新失败！");
+				logger.info("更新失败！");
 			}
-
 		} catch (Exception e) {
-			// throw new BusinessException("修改信息异常！");
 			e.printStackTrace();
-			map.put("message", "修改信息出现异常，请检查用户是否存在！");
-			return map;
 		}
+		return result;
+	}
+	
+	/**
+	 * 用户信息返回
+	 * @param id
+	 * @param request
+	 * @return
+	 */
+	@RequestMapping(value = "/userMessage")
+    @AuthLogin
+	public ResponseResult<Object> userMessage(String id , HttpServletRequest request){
+		ResponseResult<Object> result = new ResponseResult<>();
+		User user = userService.selectByPrimaryKey(Integer.parseInt(id));
+//		User user = (User) request.getSession().getAttribute("user");
+		logger.info("user:"+user);
+		result.addData(user);
+		return result;
+	}
+	
+    /**
+     * 我的投标查看
+     */
+    @RequestMapping(value = "/watchMyBid")
+    @AuthLogin
+    public ResponseResult watchMyBid (String tenderId) {
+        Map map = new HashMap();
+        Tender tender = tenderService.selectByPrimaryKey(Integer.parseInt(tenderId));
+//        Bid_order bidOrder = (Bid_order) tender;
+        map.put("tender", tender);
+        ResponseResult responseResult = new ResponseResult();
+        responseResult.addData(map);
+        return responseResult;
+    }
+    
+    /**
+     * 我的发布（带订单）
+     * @param tenderId
+     * @return
+     */
+    @RequestMapping("/selectByName")
+    public ResponseResult selectByPrimaryName(String tenderId) {
+        Map<String, Object> result = new HashMap<>();
+        Tender tender = tenderService.selectByPrimaryKey(Integer.parseInt(tenderId));
+        result.put("tender", tender);
+        System.out.println(tender);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd ");
+        String date = formatter.format(tender.getEndDate());//格式化数据
+        result.put("endDate", date);
+        Integer id = tender.getId();
+        List<Bid_order> list = bidService.selectOrderByTid(id);
+        List<OrderInfo> list1 = new ArrayList<>();
+        for (int i =0; i<list.size(); i++){
+            Integer bidInfoid = list.get(i).getBidInfoid();
+            Bid_info bid_info =  bidService.selectInfoById(bidInfoid);
+            OrderInfo orderInfo = new OrderInfo();
+            orderInfo.setBid_info(bid_info);
+            orderInfo.setBid_order(list.get(i));
+            list1.add(orderInfo);
+        }
+        result.put("olist",list1);
+        TenderStatusEnum code = TenderStatusEnum.getByCode(tender.getClassification());
+        result.put("explainl", tender.getExplainl().replaceAll("", "&nbsp;").replaceAll("\r", "<br/>"));
+        ResponseResult responseResult = new ResponseResult();
+        responseResult.addData(result);
+        return responseResult;
+    }
+    
+	/**
+	 * 加密 MD5 + 十六进制 + 盐 password = "safqwgnetrygfhehn123456j7efwhtreyguyu6y";
+	 * 
+	 * @param
+	 * @return
+	 */
+	public String encodePassword(String password) {
+		String algorithm = "MD5";
+		char[] encodeHex = null;
+		try {
+			MessageDigest instance = MessageDigest.getInstance(algorithm);
+			// MD5加密后密文
+			byte[] digest = instance.digest(password.getBytes());
+			// 十六进制再加密一次
+			encodeHex = Hex.encodeHex(digest);
+		} catch (NoSuchAlgorithmException e) {
+			e.printStackTrace();
+		}
+		return new String(encodeHex);
 	}
 }
