1575059c1d68abeb73f6ff95c38bc9555dd26930
aimeryaya <117468082@qq.com>
2018-01-30 15:33:22 +0800

    配置文件调整

--- src/test/java/com/wutong/payment/PaymentApplicationTests.java
+++ src/test/java/com/wutong/payment/PaymentApplicationTests.java
@@ -1,5 +1,8 @@
 package com.wutong.payment;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
