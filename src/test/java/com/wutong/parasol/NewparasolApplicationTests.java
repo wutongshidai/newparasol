a217a0356b29c04584b98ddf41c2714a7528a156
陈龙飚 <714147655@qq.com>
2018-01-30 15:38:20 +0800

    "商城接口"

--- src/test/java/com/wutong/parasol/NewparasolApplicationTests.java
+++ src/test/java/com/wutong/parasol/NewparasolApplicationTests.java
@@ -1,4 +1,4 @@
-package com.wutong.payment;
+package com.wutong.parasol;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PaymentApplicationTests {
+public class NewparasolApplicationTests {
 
 	@Test
 	public void contextLoads() {
